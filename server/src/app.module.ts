78304884af5e75b60c526b173bb228fd47526b7d
Pavel Krokhmal <pavel.krokhmal.mn@outlook.com>
2021-07-31 14:34:24 +0300

    mongoDB, schemes, REST API, track & comment DTO

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
-import {Module} from '@nestjs/common'
-import {TrackModule} from "./track/track.module";
+import { Module } from '@nestjs/common';
+import { TrackModule } from './track/track.module';
+import {MongooseModule} from "@nestjs/mongoose";
 
 @Module({
-    imports: [TrackModule]
+  imports: [
+      TrackModule,
+      MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.4lxa4.mongodb.net/music-platform?retryWrites=true&w=majority')
+  ],
 })
-export class AppModule {
-
-}
+export class AppModule {}
