fe7783e5f4253df1f1c1e0a7f39bfd567578e759
Pavel Krokhmal <pavel.krokhmal.mn@outlook.com>
2021-07-31 16:31:23 +0300

    file uploading, static, track search

--- server/src/track/track.module.ts
+++ server/src/track/track.module.ts
@@ -4,6 +4,7 @@ import { TrackService } from './track.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Track, TrackSchema } from './schemes/track.schema';
 import { Comment, CommentSchema } from './schemes/comment.schema';
+import { FileService } from '../file/file.service';
 
 @Module({
   imports: [
@@ -11,6 +12,6 @@ import { Comment, CommentSchema } from './schemes/comment.schema';
     MongooseModule.forFeature([{ name: Comment.name, schema: CommentSchema }]),
   ],
   controllers: [TrackController],
-  providers: [TrackService],
+  providers: [TrackService, FileService],
 })
 export class TrackModule {}
