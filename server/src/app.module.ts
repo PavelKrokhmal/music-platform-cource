b6f7dd72d1d8817d15f0e8b7df4c28230c8dbcde
Pavel Krokhmal <pavel.krokhmal.mn@outlook.com>
2021-08-08 16:34:50 +0300

    added .env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,16 +4,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { FileModule } from './file/file.module';
 import * as path from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: path.resolve(__dirname, 'static'),
     }),
     TrackModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://admin:admin@cluster0.4lxa4.mongodb.net/music-platform?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRoot(process.env.MONGO_URL),
     FileModule,
   ],
 })
